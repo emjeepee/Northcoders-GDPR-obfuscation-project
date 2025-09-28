aaaa4119d52905587ac4b1cfc1d88c2d0ca018c7
Mukund Pandit <mukund.pandit@virginmedia.com>
2025-09-28 23:35:15 +0100

    Start creation of function process_csv.

--- obf_lib/get_file.py
+++ obf_lib/get_file.py
@@ -15,14 +15,14 @@ def get_file(bucket: str, key: str):
           name of the bucket.
         key: a string that is the key
           under which the S3 bucket stores 
-          file that contains the data, 
+          the file that contains the data, 
           some of which will be 
           obfuscated in a different
           function.
 
     Returns:
         The file that the S3 bucket bucket  
-        stores under the key key.
+        stores under key key.
     """
     
 
--- obf_lib/process_csv.py
+++ obf_lib/process_csv.py
@@ -6,6 +6,27 @@ import csv
 
 
 def process_csv(file, pii_fields):
+    """
+    This function:
+        looks in the passed-in file for 
+          the given PII fields and 
+          obfuscates the data under those
+          fields.
+    
+    Arguments:
+        file: the .csv file that previous 
+          code has read from the S3 bucket.
+        pii_filed: a list of strings, each
+          string being a field in the .csv
+          file.
+
+    Returns:
+        a byte stream in which code has 
+          obfuscated the data under the 
+          given PII fields.
+    """
+
+
     input_stream = io.StringIO(file)
     reader = csv.DictReader(input_stream)
 
@@ -15,8 +36,7 @@ def process_csv(file, pii_fields):
 
     for row in reader:
         for field in pii_fields:
-            if field in row:
-                row[field] = "***"
+            row[field] = "***" if field in row else row.get(field)        
         writer.writerow(row)
 
     # Convert to byte stream
